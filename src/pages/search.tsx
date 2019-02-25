c11cbd9e18ce37b1afbb6da143dc46c46a5d6ab4
Sam Bokai <sam.bokai@scout24.com>
2019-02-25 15:18:17 +0100

    Implement search query on form submit using ApolloConsumer

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {ChangeEvent, Component, FormEvent} from 'react';
 import PersonLink from "../components/PersonLink";
 
-import {Query} from "react-apollo";
+import {ApolloConsumer} from "react-apollo";
 import gql from "graphql-tag";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 import {Person} from "../types/types";
+import ApolloClient from "apollo-client/ApolloClient";
 
-class AllPersonsQuery extends Query<{
-    allPersons:  Array<Person>,
-}, {}> {}
 
-const getAllPersons = gql`{
-    allPersons(filter: {species_some: {name: "Human"}}) {
+interface SearchAllPersonsResult { allPersons: Array<Person> }
+interface SearchAllPersonsVariables {queryString: string;}
+
+const searchAllPersons = gql`query SearchPersonsQuery($queryString: String!) {
+    allPersons(filter: {
+        name_contains: $queryString
+        AND: {species_some: {name: "Human"}}
+    }) {
         id
         name
         homeworld {
@@ -21,29 +25,76 @@ const getAllPersons = gql`{
     }
 }`;
 
-export const Search = () => {
-    return (
-        <section>
-            <form>
-                <input type="text" placeholder="Skywalker"/>
-                <button type="submit">Search</button>
-            </form>
+
+interface State {
+    searchInput: string;
+    searchResults?: Array<Person>
+    loading: boolean;
+    error: boolean;
+}
+
+export class Search extends Component<{}, State> {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            searchInput: '',
+            loading: false,
+            error: false,
+        }
+    }
 
 
-            <ul>
-                <AllPersonsQuery query={getAllPersons}>
-                    {({loading, error, data}) => {
-                        if (loading) return <Loading/>;
-                        if (error ||!data) return <Error/>;
+    render() {
+        const {searchResults, loading, error} = this.state;
+        return (
+            <section>
+                <ApolloConsumer>
+                    {(client) => (
+                        <form onSubmit={event => this.handleSubmit(event, client)}>
+                            <input type="text" placeholder="Skywalker" value={this.state.searchInput}
+                                   onChange={this.handleSearchInputChange}/>
 
-                        return data.allPersons.map(({id, name}) =>
-                            <li key={id}>
-                                <PersonLink id={id} name={name}/>
-                            </li>
-                        );
-                    }}
-                </AllPersonsQuery>
-            </ul>
-        </section>
-    )
-};
+                            <button type="submit">Search</button>
+                        </form>
+                    )}
+                </ApolloConsumer>
+
+
+                {loading && <Loading/>}
+                {error && <Error/>}
+
+                <ul>
+                    {searchResults && searchResults.map(({id, name}, key) =>
+                        <li key={key}>
+                            <PersonLink id={id} name={name}/>
+                        </li>
+                    )}
+                </ul>
+            </section>
+        )
+    }
+
+    private handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        this.setState({searchInput: event.target.value})
+    };
+
+    private async handleSubmit(event: FormEvent, client: ApolloClient<any>) {
+        event.preventDefault();
+        this.setState({loading: true});
+
+        client.query<SearchAllPersonsResult, SearchAllPersonsVariables>({
+            query: searchAllPersons,
+            variables: {queryString: this.state.searchInput}
+        })
+            .then(({data}) => {
+                return this.setState({searchResults: data.allPersons});
+            })
+            .catch((error: any) => {
+                console.log(error);
+                return this.setState({error: true});
+            })
+            .finally(() => this.setState({loading: false}));
+
+    };
+}
